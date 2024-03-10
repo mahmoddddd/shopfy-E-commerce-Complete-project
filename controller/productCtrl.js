eac4aa07e437aa780a2efe4a8999c141c5185497
mahmod <alshryfmhmwd668@gmail.com>
2024-03-10 13:40:06 +0200

    add Sorting Functionality

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -3,6 +3,8 @@ const slugify = require('slugify')
 const asyncHandler = require('express-async-handler');
 const Product = require('../models/productModel');
 
+
+
 // creat product 
 const createProduct = asyncHandler(async (req, res) => {
     try {
@@ -10,26 +12,31 @@ const createProduct = asyncHandler(async (req, res) => {
         if (req.body.title) {
             req.body.slug = slugify(req.body.title)
         }
-        // const product = new Product({
-        //     price: req.body.price,
-        //     description: req.body.description,
-        //     slug: req.body.slug,
-        //     title: req.body.title,
-        //     quantity: req.body.quantity,
 
-        // });
         const product = new Product(req.body)
+
         const exsistingProduc = await Product.findOne({ slug: product.slug })
-        if (!exsistingProduc) {
+
+
+        if (exsistingProduc) {
+            throw new Error('product is already exsist')
+
+        } else {
             const savedProduct = await product.save();
             res.status(201).json(savedProduct);
+
         }
-        throw new Error('product is already exsist')
 
     } catch (error) {
         throw new Error(error);
     }
 });
+
+
+
+
+
+
 
 const getAproduct = asyncHandler(async (req, res) => {
     const { id } = req.params;
@@ -89,29 +96,35 @@ const deleteProduct = asyncHandler(async (req, res) => {
 
 })
 
+
 const getAllProducts = asyncHandler(async (req, res) => {
     const allProductSearchIn = req.query;
-
     try {
+        // Filtering
         const queryObj = { ...req.query };
         const excludeFields = ["page", "sort", "limit", "fields"];
+        excludeFields.forEach(el => delete queryObj[el]);
 
-        excludeFields.forEach(el => delete queryObj[el]); // Corrected forEach syntax
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
 
-        console.log(queryObj, req.query);
+        // Execute the Query
+        const query = Product.find(JSON.parse(queryStr)); // Corrected typo here
 
-        const allProducts = await Product.find(allProductSearchIn);
-
-        if (allProducts.length === 0) {
-            return res.status(404).json('No products found');
+        // Sorting 
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort('')
         }
 
-        res.json(allProducts);
+
+        // Execute the Query and Send Response
+        const product = await query;
+        res.json(product);
     } catch (error) {
         throw new Error(error);
     }
 });
-
 
 
 module.exports = {
